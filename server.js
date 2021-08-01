b9639db8e6ab0bb6b4a71ee9ac6076693b7b77a4
olowookere marvellous <olowomarve@gmail.com>
2021-08-01 16:42:39 -0700

    installed pusher package

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use(express.json())
 // api routes
 app.get("/", (req, res) => res.status(200).send("Hello world"));
 
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+});
+
 app.post('/messages/new', (req, res) => {
     const dbMessage = req.body;
     Messages.create(dbMessage, (err, data) => {
